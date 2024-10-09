16165a70c5e704718ecb06af5538b2135a3e57f0
Oleg <35769958+trudoholic@users.noreply.github.com>
2024-10-09 22:23:49 +0300

    onDocumentKey

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,3 +1,4 @@
+import {useEffect} from "react"
 import useAppContext from "../context/useAppContext"
 import {Actions} from "../context/reducer"
 import {IState} from "../context/state"
@@ -15,6 +16,26 @@ const useGame = () => {
   const decCount = (n: number) => {
     dispatch({type: Actions.SetCount, payload: count - n})
   }
+
+  useEffect(() => {
+    const onDocumentKey = (e) => {
+      // console.log(e.key, e.code)
+      switch(e.code) {
+        case "ArrowUp": {
+          incCount(1)
+          break
+        }
+        case "ArrowDown": {
+          decCount(1)
+          break
+        }
+      }
+    }
+    document.addEventListener('keyup', onDocumentKey)
+    return () => {
+      document.removeEventListener('keyup', onDocumentKey)
+    }
+  }, [incCount, decCount])
 
   return {
     count,
