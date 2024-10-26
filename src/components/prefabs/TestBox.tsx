f9e14045d88369fd4712b8b07f591d0f29a27587
Oleg <35769958+trudoholic@users.noreply.github.com>
2024-10-26 14:25:18 +0300

    TestBox offset

--- src/components/prefabs/TestBox.tsx
+++ src/components/prefabs/TestBox.tsx
@@ -1,10 +1,12 @@
+import {a, config, useSpring} from "@react-spring/three"
 import {Edges} from "@react-three/drei"
+import {offset} from "../../hooks/utils"
 
 export function TestBox() {
   return (
     <mesh
       name={"TestBox"}
-      position={[3, 0, 3]}
+      position={[3 - offset.x, 0, 3 - offset.y]}
       scale={[.75, 1.5, .75]}
       castShadow={true}
       receiveShadow={true}
