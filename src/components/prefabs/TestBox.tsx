528c45874b84ac848c55cd405c17c2cef0c5193b
Oleg <35769958+trudoholic@users.noreply.github.com>
2024-10-26 14:41:48 +0300

    useSpring

--- src/components/prefabs/TestBox.tsx
+++ src/components/prefabs/TestBox.tsx
@@ -1,20 +1,35 @@
-import {a, config, useSpring} from "@react-spring/three"
+import {a, useSpring} from "@react-spring/three"
 import {Edges} from "@react-three/drei"
 import {offset} from "../../hooks/utils"
 
 export function TestBox() {
+
+  const myCallback = () => {
+    console.log("animation complete");
+  }
+  const {x} = useSpring({
+    from: { x: 0 },
+    to: { x: 3 },
+    config: {duration: 1500},
+    onRest: myCallback
+  })
+
   return (
-    <mesh
-      name={"TestBox"}
-      position={[3 - offset.x, 0, 3 - offset.y]}
-      scale={[.75, 1.5, .75]}
-      castShadow={true}
-      receiveShadow={true}
+    <a.group
+      position-x={x}
     >
-      <boxGeometry />
-      <meshStandardMaterial color={"orange"} fog={false} />
-      <Edges color={"white"} linewidth={.5} />
-    </mesh>
+      <mesh
+        name={"TestBox"}
+        position={[3 - offset.x, 0, 3 - offset.y]}
+        scale={[.75, 1.5, .75]}
+        castShadow={true}
+        receiveShadow={true}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color={"orange"} fog={false} />
+        <Edges color={"white"} linewidth={.5} />
+      </mesh>
+    </a.group>
   )
 }
 
